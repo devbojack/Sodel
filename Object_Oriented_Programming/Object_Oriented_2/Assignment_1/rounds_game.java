48e49b787ba7b1e239a93d200f7eeefcf0247f48
Justus Lolwerikoi <98508704+devbojack@users.noreply.github.com>
2026-01-14 11:57:52 +0300

    Implement rounds-based scoring game
    Added a rounds-based game where users enter a lucky number and receive scores based on their input compared to a randomly generated secret number.

--- Object_Oriented_Programming/Object_Oriented_2/Assignment_1/rounds_game.java
+++ Object_Oriented_Programming/Object_Oriented_2/Assignment_1/rounds_game.java
@@ -1,1 +1,50 @@
+import java.util.Scanner;
+import java.util.Random;
 
+/*
+// RoundsGame
+// Game that allows users to enter
+// a number a awards them depending
+// on the win, draw or lose
+*/
+
+public class RoundsGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+
+        int totalScore = 0;
+
+        System.out.print("Enter number of rounds: ");
+        int rounds = scanner.nextInt();
+
+        for (int i = 1; i <= rounds; i++) {
+            System.out.print("Enter lucky number for round " + i + ": ");
+             int secretNumber = rand.nextInt(9) + 1;
+            int luckyNumber = scanner.nextInt();
+
+            int remainder = luckyNumber % secretNumber;
+
+            if (remainder == 0) {
+                totalScore += 1;
+                System.out.println("You Draw! Score : +1");
+            } else if (remainder % 2 == 0) {
+                totalScore += 3;
+                System.out.println("You Win! Score : +3");
+            } else {
+                totalScore -= 3;
+                System.out.println("You Lose! Score : -3");
+            }
+        }
+
+        System.out.println("Total Score: " + totalScore);
+
+        if (totalScore > 0) {
+            System.out.println("You won the game.");
+        } else {
+            System.out.println("You lost the game.");
+        }
+
+        scanner.close();
+    }
+}
